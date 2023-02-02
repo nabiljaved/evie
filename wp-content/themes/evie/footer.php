2557ebc6079d3feed2041c128c9569e79af916a1
nabiljaved <nabeeljaved2016@gmail.com>
2023-02-02 18:10:32 +0400

    almost eighty percent

--- wp-content/themes/evie/footer.php
+++ wp-content/themes/evie/footer.php
@@ -27,11 +27,17 @@
 						<p>Subscribe to our newsletter and never miss the new updates.</p>
 					</div>
 					<div class="upper_footer_section_inner_right">
-					      <input size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Your email address" value="" type="email" name="email-278">
-						  <input style="margin-left:10px" class="wpcf7-form-control has-spinner wpcf7-submit" type="submit" value="Subscribe">
+					      <!-- <form class="upper_footer_section_inner_right"> -->
+						  	<input size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Your email address" value="" type="email" name="email-278">
+						  	<input style="margin-left:10px" class="wpcf7-form-control has-spinner wpcf7-submit" type="submit" value="Subscribe">
+						  <!-- </form> -->
 				    </div>
 				</div>
 			</div>
+
+				
+
+			
 
 				<?php
 
